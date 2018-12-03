dad0f11403f39ac27939ddfb4b524f0457a3dee1
Jonathan Remmers <jona.remm@gmail.com>
2018-12-03 03:12:11 +0100

    starting partial solution to part two

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -1,11 +1,22 @@
 def main():
-    frequency = 0
+    frequency   = 0
+    variations  = []
+    match_found = False
 
+    # while not match_found:
     with open("input.txt") as f:
         for line in f:
-            num = int(line.strip())
-            frequency += num
+            change = int(line.strip())
+            frequency += change
+            variations.append(frequency)
+            print(frequency)
+            # if frequency not in variations:
+            #     variations.append(frequency)
+            # else:
+            #     match_found = True
+            #     print("Answer to part 2: " + str(frequency))
+            #     break
 
-    print(frequency)
+    print("Answer to part 1: " + str(frequency))
 
 main()
